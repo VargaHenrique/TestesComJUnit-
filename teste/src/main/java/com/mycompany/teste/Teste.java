8be5e793989658fff82d6d554b0f469d37b4c34f
Henrique Varga <119634135+VargaHenrique@users.noreply.github.com>
2023-12-06 22:02:34 -0300

    Add files via upload

---
+++ teste/src/main/java/com/mycompany/teste/Teste.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teste;
+
+/**
+ *
+ * @author Username
+ */
+public class Teste {
+
+    public int metodoSoma(){
+    
+    int numero1 = 4;
+    int numero2 = 4;
+    
+    int soma = numero1 + numero2;
+    
+    return soma;
+    
+    
+    }
+    
+    public int metodoSub(){
+    
+    int numero1  = 9;
+    int numero2 = 9; 
+    
+    int sub = numero1 - numero2;
+    
+    return sub;
+    
+    }
+    
+     public int metodoMulti(){
+    
+    int numero1  = 5;
+    int numero2 = 4; 
+    
+    int multi = numero1 * numero2;
+    
+    return multi;
+    
+    }
+     
+     public int metedoDivisao(){
+     
+     int numero1 = 2;
+     int numero2 = 7;
+     
+     int divi = numero1/numero2;
+     
+     return divi;
+     
+     }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+  
+}
+
